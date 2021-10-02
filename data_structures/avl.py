ced3a160626fb42dc7bdc669729f8a562100c2b7
ra7kar <ra7kar@gmail.com>
2021-10-01 23:29:38 -0500

    Add final AVLTree code to avl.py

--- data_structures/avl.py
+++ data_structures/avl.py
@@ -3,11 +3,11 @@
 import random
 
 class Node:
-    def __init__(self, data, left=None, right=None, height=0):
+    def __init__(self, data, left=None, right=None):
         self.data = data
         self.left = left
         self.right = right
-        self.height = height
+        self.height = 1
 
     def __str__(self):
         lines, *_ = self._display_aux()
@@ -71,28 +71,125 @@ class AVLTree:
         if self.root is None:
             self.root = Node(data)
         else:
-            self._insert(self.root, data)
+            self.root = self._insert(self.root, data)
         print("="*30 + str(data) + "="*30)
         print(self)
 
     def _insert(self, subroot, data):
 
+        if data == subroot.data:
+            return subroot
+
         if data < subroot.data:
             if subroot.left is None:
                 subroot.left = Node(data)
             else:
-                self._insert(subroot.left, data)
+                subroot.left = self._insert(subroot.left, data)
         elif data > subroot.data:
             if subroot.right is None:
                 subroot.right = Node(data)
             else:
-                self._insert(subroot.right, data)
+                subroot.right = self._insert(subroot.right, data)
 
         # Update heights
         height_left = subroot.left.height if subroot.left else 0
         height_right = subroot.right.height if subroot.right else 0
         subroot.height = max(height_left, height_right) + 1
 
+        # assing the new root to return.
+        new_root = subroot
+     
+        # check if imbalance exist
+        if abs(height_left-height_right) > 1:
+            new_root = self._fix_imbalance(subroot, height_left, height_right)
+
+        return new_root
+
+    def _fix_imbalance(self, subroot, height_left, height_right):
+        if height_left > height_right:  # left heavy
+            
+            # get left , left and right heights
+            left_left_height = subroot.left.left.height if subroot.left.left else 0
+            left_right_height = subroot.left.right.height if subroot.left.right else 0
+
+            if left_left_height > left_right_height : # left left heavy
+                #right rotate
+                new_root = AVLTree._right_rotate(subroot)
+            else:
+                # left right rotate left right heavy
+                new_root = AVLTree._left_right_rotate(subroot)
+
+        else:
+
+            right_left_height = subroot.right.left.height if subroot.right.left else 0
+            right_right_height = subroot.right.right.height if subroot.right.right else 0
+
+            if right_right_height > right_left_height: # right right heavy
+                #left rotate
+                new_root = AVLTree._left_rotate(subroot)
+            else:
+                # right left rotate    right left heavy
+                new_root = AVLTree._right_left_rotate(subroot)
+
+        AVLTree._fix_height(new_root)
+
+        return new_root
+
+
+
+    @staticmethod
+    def _right_rotate(subroot):
+        new_root = subroot.left
+        subroot.left = new_root.right
+        new_root.right = subroot
+
+
+
+        return new_root
+    
+    @staticmethod
+    def _left_right_rotate(subroot):
+        new_root = subroot.left.right
+        subroot.left.right = new_root.left
+        subroot.left.height = 1
+        new_root.left = subroot.left
+        subroot.left = new_root.right
+        new_root.right = subroot
+
+        return new_root
+
+    def _left_rotate(subroot):
+        
+        new_root = subroot.right
+        subroot.right = new_root.left
+        new_root.left = subroot
+
+        return new_root
+
+    @staticmethod
+    def _right_left_rotate(subroot):
+
+        new_root = subroot.right.left
+        subroot.right.left = new_root.right
+        subroot.right.height = 1
+        new_root.right = subroot.right
+        subroot.right = new_root.left
+        new_root.left = subroot
+
+        return new_root
+
+    @staticmethod
+    def _fix_height(subroot):
+        if subroot is None:
+            return 0
+
+        left_height = AVLTree._fix_height(subroot.left)
+        right_height = AVLTree._fix_height(subroot.right)
+        
+        subroot.height = max(left_height, right_height) + 1
+        
+        return subroot.height
+    
     def __str__(self):
         return str(self.root)
 
@@ -109,15 +206,15 @@ class AVLTree:
         yield from _inorder_traversal(self.root)
 
 
-
-ll = []
+ll = [] #[6,3,7,1,4,5]
 t = AVLTree(ll)
 
 if len(ll) == 0:
-    insert_list = [random.randint(0, 1000) for _ in range(10)]
-    print("Insert list: {}".format(insert_list))
+    insert_list = [random.randint(0, 10) for _ in range(10)]
 
     for i in insert_list:
         t.insert(i)
 
-# print(avltree)
+    print("Insert list: {}".format(insert_list))
+
+#print(t)
