2cb8dedcd5027b8432f69c3670a18f6c6a8004ee
ra7kar <ra7kar@gmail.com>
2021-11-05 17:49:51 -0500

    Code review cleanup done

--- sudoku/validate_sudoku.py
+++ sudoku/validate_sudoku.py
@@ -1,104 +1,64 @@
-# validate sudoku
-# input is a board
-# Return True if board is correct
-# Return False if board is incorrect
+""" validate sudoku board
+    input is a board
+    Return True if board is correct
+    Return False if board is incorrect
+"""
 
 
-def validate(board):
+def validate_sudoko_board(board):
 
     board_len = len(board)
 
     # # check row numbers for duplicates
     seen = {}
-    dups = []
-    for row in range(board_len):
-        #print( bo[i])
-        for col in board[row]:
-            
+    for row in board:
+        for col in row:
             if col not in seen:
                 seen[col] = 1
             else:
-                dups.append(col)
+                return False
             
         seen={}
-        if len(dups) > 0:
-            return False
     
     # check columns for duplicates
     seen = {}
-    dups = []
     for col in range(board_len):
         for row in board:
-            print(row[col])
             if row[col] not in seen:
                 seen[row[col]] = 1
             else:
-                dups.append(row[col])
+                return False
+ 
         seen = {}
-        if len(dups) > 0:
-            return False
-        print("--------")
-    
 
     # check the box for duplicates
-    for row in range(len(board)):
+    for row in range(board_len):
         first_box = {}
         second_box = {}
         third_box = {}
-        if row % 3 == 0 and row !=0:
-            print("-"*30)
-        for col in range(len(board[0])):
+        for col in range(board_len):
             val = board[row][col]
             square = col // 3
-
             if square == 0 :
                 if val in first_box:
-                    print("Duplcate in one")
                     return False
                 else:
                     first_box[val]=1
 
             if square == 1 :
                 if val in second_box:
-                    print("Duplcate in second")
                     return False
                 else:
                     second_box[val]=1
 
             if square == 2 :
                 if val in third_box:
-                    print("Duplcate in third")
                     return False
                 else:
                     third_box[val]=1
 
-            if col % 3 == 0 and col != 0:
-                print("| ", end="") 
-                     
-            if col == 8:
-                print(board[row][col])
-            else:
-                print(str(board[row][col]) + " ", end=" ")
-            
-
-
     return True
         
-def print_board(board):
-    board_len = len(board)
-
-    for row in range(board_len):
-        
-        if row % 3 == 0 and row != 0:
-            print( "-"*32) 
-        
-        # for col in range(len(board[0])):
-            
-        #     if col%3 == 0 and col != 0:
-        #         print((board[row][col]))
-        #     else:
-        #         print(board[row][col], end=" ")
-
 
 board = [[  7,  8,  5, 4,  3,  9, 1,  2,  6],
          [  6,  1,  2, 8,  7,  5, 3,  4,  9],
@@ -111,9 +71,12 @@ board = [[  7,  8,  5, 4,  3,  9, 1,  2,  6],
          [  3,  4,  9, 2,  1,  6, 8,  5,  7]]
 
 
-result = validate(board)
-print(result)
-#print_board(board)
+result = validate_sudoko_board(board)
+print(""*2)
+print("-"*32)
+print("Sudoku board FAILED test" if not result else "Sudoku Board PASSED the test")
+print("-"*32)
+print("")
 
 
 
