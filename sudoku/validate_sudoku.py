3640b20e2c5512ff98b93d8260ef29a89ea7c21d
ra7kar <65131599+ra7kar@users.noreply.github.com>
2021-11-06 16:17:32 -0500

    Add sudoku board validator (#11)
    * Add sudoku board validator
    * Add tests to validate sudoku checker

---
+++ sudoku/validate_sudoku.py
@@ -0,0 +1,74 @@
+# Validate sudoku board
+
+from math import sqrt
+from pprint import pprint
+
+
+def validate_sudoku_board(board):
+    """Check if a sudoku board is valid.
+
+    Args:
+        board (list[list[int]]): Board specified as a 2d array.
+
+    Returns:
+        boolean: Return True if board is valid, else return False.
+    """
+
+    board_len = len(board)
+
+    # # check for duplicate numbers in ROW
+    for row in board:
+        seen = set()
+        for col in row:
+            if col in seen:
+                return False
+            seen.add(col)
+
+    # check for duplicates numbers in COLUMNS
+    for col_idx in range(board_len):
+        seen = set()
+        for row in board:
+            if row[col_idx] in seen:
+                return False
+            seen.add(row[col_idx])
+
+    # check the 3x3 box for duplicates in a 9x9 board
+    box_size = int(sqrt(board_len))  # box size is 3 x 3
+    for row_box in range(box_size):
+        for col_box in range(box_size):
+            seen = set()
+            for row in range(row_box * box_size, (row_box + 1) * box_size):
+                for col in range(col_box * box_size, (col_box + 1) * box_size):
+                    val = board[row][col]
+                    if val in seen:
+                        return False
+                    seen.add(val)
+
+    return True
+
+
+if __name__ == "__main__":
+
+    board = [
+        [7, 8, 5, 4, 3, 9, 1, 2, 6],
+        [6, 1, 2, 8, 7, 5, 3, 4, 9],
+        [4, 9, 3, 6, 2, 1, 5, 7, 8],
+        [8, 5, 7, 9, 4, 3, 2, 6, 1],
+        [2, 6, 1, 7, 5, 8, 9, 3, 4],
+        [9, 3, 4, 1, 6, 2, 7, 8, 5],
+        [5, 7, 8, 3, 9, 4, 6, 1, 2],
+        [1, 2, 6, 5, 8, 7, 4, 9, 3],
+        [3, 4, 9, 2, 1, 6, 8, 5, 7],
+    ]
+
+    result = validate_sudoku_board(board)
+
+    print("" * 2)
+    print("-" * 32)
+    print("Sudoku board FAILED test" if not result else "Sudoku Board PASSED the test")
+    print("-" * 32)
+    print("")
+    print("Pretty print the board")
+    print("-" * 30)
+
+    pprint(board)
