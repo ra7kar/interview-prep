9ed698f55af4eedd1aad245c976298fd533553c1
ra7kar <ra7kar@gmail.com>
2021-11-05 21:56:33 -0500

    Code cleanup and added comments

--- sudoku/validate_sudoku.py
+++ sudoku/validate_sudoku.py
@@ -1,64 +1,63 @@
-""" validate sudoku board
-    input is a board
-    Return True if board is correct
-    Return False if board is incorrect
-"""
 
+# Validate sudoku board
+
+from math import sqrt
+from pprint import pprint
 
 def validate_sudoko_board(board):
+    """ 
+    Return True if board is correct
+    Return False if board is incorrect
+    """ 
 
     board_len = len(board)
 
-    # # check row numbers for duplicates
-    seen = {}
+    # # check for duplicate numbers in ROW
     for row in board:
+        seen = set()
         for col in row:
-            if col not in seen:
-                seen[col] = 1
-            else:
+            if col in seen:
                 return False
-            
-        seen={}
+            seen.add(col)
     
-    # check columns for duplicates
-    seen = {}
-    for col in range(board_len):
+    # check for duplicates numbers in COLUMNS
+    for col_idx in range(board_len):
+        seen = set()
         for row in board:
-            if row[col] not in seen:
-                seen[row[col]] = 1
-            else:
+            if row[col_idx] in seen:
                 return False
- 
-        seen = {}
+            seen.add(row[col_idx])
 
-    # check the box for duplicates
-    for row in range(board_len):
-        first_box = {}
-        second_box = {}
-        third_box = {}
-        for col in range(board_len):
-            val = board[row][col]
-            square = col // 3
-            if square == 0 :
-                if val in first_box:
-                    return False
-                else:
-                    first_box[val]=1
-
-            if square == 1 :
-                if val in second_box:
-                    return False
-                else:
-                    second_box[val]=1
-
-            if square == 2 :
-                if val in third_box:
-                    return False
-                else:
-                    third_box[val]=1
+    # check the 3x3 box for duplicates in a 9x9 board
+    box_size = int(sqrt(board_len))   # box size is 3 x 3
+    for row_box in range(box_size):
+        for col_box in range(box_size):
+            seen = set() 
+            for row in range(row_box*box_size, (row_box+1)*box_size):
+                for col in range(col_box*box_size, (col_box+1)*box_size):
+                    val = board[row][col]
+                    if val in seen:
+                        return False
+                    seen.add(val)
 
     return True
-        
+
+def print_board(board):
+    board_len = len(board)
+    for row in range(board_len):
+        if row % 3 == 0 and row != 0:
+            print("-"*25)
+        for col in range(board_len):
+
+            if col % 3 == 0 and col !=0 :
+                print(" | ", end=" ")
+          
+            if col == 8:
+                print(board[row][col])
+            else:
+                print(board[row][col], end=" ")
+            
+
 
 board = [[  7,  8,  5, 4,  3,  9, 1,  2,  6],
          [  6,  1,  2, 8,  7,  5, 3,  4,  9],
@@ -70,13 +69,23 @@ board = [[  7,  8,  5, 4,  3,  9, 1,  2,  6],
          [  1,  2,  6, 5,  8,  7, 4,  9,  3],
          [  3,  4,  9, 2,  1,  6, 8,  5,  7]]
 
-
 result = validate_sudoko_board(board)
+
 print(""*2)
 print("-"*32)
 print("Sudoku board FAILED test" if not result else "Sudoku Board PASSED the test")
 print("-"*32)
 print("")
+print( "Pritty print the board")
+print( "-"*30)
+
+print( "Test print the Board with grids")
+pprint(board)
+print("")
+print( "Test print the Board with grids")
+print( "-"*32)
+print_board(board)
+
 
 
 
