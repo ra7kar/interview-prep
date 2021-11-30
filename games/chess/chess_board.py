c1b13bc30cb6d00da1ed0e353b3a6abafba29200
ra7kar <65131599+ra7kar@users.noreply.github.com>
2021-11-29 19:48:30 -0600

    Add chess pieces and tests (#15)
    * Add chess piece valid moves
    
    * Move sudoku to games folder
    
    * Update review changes
    
    * Input of Row and Col values from Terminal
    
    * Code review comments are fixed
    
    * Update comment in games/chess/chess_board.py
    
    Co-authored-by: atrifex <12935724+atrifex@users.noreply.github.com>

---
+++ games/chess/chess_board.py
@@ -0,0 +1,167 @@
+# With respect to position, list valid chess piece moves
+
+# External
+from abc import ABCMeta, abstractmethod
+
+
+def is_valid_position(row, col):
+    """Validate if row/col are within limits of chess board
+
+    Args:
+        row (int): row position of the chess piece
+        col (int): column position of the chess piece
+
+    Returns:
+        boolean: if valid return True, else False
+    """
+
+    if row > 8 or row < 1:
+        return False
+
+    if col > 8 or col < 1:
+        return False
+
+    return True
+
+
+class ChessPiece(metaclass=ABCMeta):
+    """Abstract base class of Chess piece
+    metaclass derived from ABCMeta
+    """
+
+    def __init__(self, row, col):
+
+        if row > 8 or row < 1:
+            raise ValueError("Row is not in valid range")
+        self.row = row
+
+        if col > 8 or col < 1:
+            raise ValueError("Col is not in valid range")
+        self.col = col
+
+    def get_position(self):
+        return (self.row, self.col)
+
+    @abstractmethod
+    def get_valid_moves(self):
+        raise NotImplementedError
+
+    @property
+    def name(self):
+        return type(self).__name__
+
+
+class King(ChessPiece):
+    """King chess piece
+
+    Args:
+        ChessPiece (ABC): ABC for chess piece
+    """
+
+    def get_valid_moves(self):
+        row, col = self.get_position()
+        valid_moves = []
+
+        for r_shift in [-1, 0, 1]:
+            for c_shift in [-1, 0, 1]:
+
+                new_r = row + r_shift
+                new_c = col + c_shift
+
+                if is_valid_position(new_r, new_c):
+                    if (new_r, new_c) != (row, col):
+                        valid_moves.append((new_r, new_c))
+
+        return valid_moves
+
+
+class Bishop(ChessPiece):
+    """Bishop chess piece
+
+    Args:
+        ChessPiece (ABC): ABC of chess piece
+    """
+
+    def get_valid_moves(self):
+        row, col = self.get_position()
+        valid_moves = []
+
+        for r_shift in [-1, 1]:
+            for c_shift in [-1, 1]:
+
+                new_r = row
+                new_c = col
+
+                while is_valid_position(new_r, new_c):
+                    if (new_r, new_c) != (row, col):
+                        valid_moves.append((new_r, new_c))
+
+                    new_r = new_r + r_shift
+                    new_c = new_c + c_shift
+
+        return valid_moves
+
+
+class Queen(ChessPiece):
+    """Queen chess piece"""
+
+    def get_valid_moves(self):
+        valid_moves = []
+        row, col = self.get_position()
+
+        for r_shift in [-1, 0, 1]:
+            for c_shift in [-1, 0, 1]:
+                if r_shift == 0 and c_shift == 0:
+                    continue
+
+                new_r, new_c = row, col
+
+                while is_valid_position(new_r, new_c):
+                    if (new_r, new_c) != (row, col):
+                        valid_moves.append((new_r, new_c))
+
+                    new_r = new_r + r_shift
+                    new_c = new_c + c_shift
+
+        return valid_moves
+
+
+# ----------
+if __name__ == "__main__":
+
+    row, col = int(input("Enter row : ")), int(input("Enter col : "))
+
+    print("-" * 75)
+    king = King(row, col)
+    print("")
+    print("Initial Position of the " + king.name + " is : " + str(king.get_position()))
+    print("-" * 40)
+    print("Valid moves for the " + king.name + " is : ", end="")
+    print(king.get_valid_moves())
+    print("-" * 75)
+    print("")
+    # --------------------
+    print("")
+    bishop = Bishop(row, col)
+    print(
+        "Initial Position of the " + bishop.name + " is : " + str(bishop.get_position())
+    )
+    print("-" * 40)
+
+    print("Valid moves for the " + bishop.name + " is : ", end="")
+    print(bishop.get_valid_moves())
+    print("-" * 75)
+    print("")
+    # --------------------
+    print("")
+    queen = Queen(row, col)
+    print(
+        "Initial Position of the " + queen.name + " is : " + str(queen.get_position())
+    )
+    print("-" * 40)
+
+    print("Valid moves for the " + queen.name + " is : ", end="")
+    print(queen.get_valid_moves())
+    print("-" * 75)
+    print("")
+    # ------------------------------
