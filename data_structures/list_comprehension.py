5e21310812872b6a57d6c40aa05185be984cd483
ra7kar <ra7kar@gmail.com>
2021-12-03 15:57:02 -0600

    Comments/ Documenation added in the file

--- data_structures/list_comprehension.py
+++ data_structures/list_comprehension.py
@@ -1,19 +1,19 @@
 # list compression
 
 
-# def list_comp(x,y,z,n):
-#     l_list = []
-#     for i in range(x+1):
-#         for j in range(y+1):
-#             for k in range(z+1):
-#                 if i+j+k != n:
-#                     l_list.append([i,j,k])
-
-#     return l_list
-
-
 def list_comp(x, y, z, n):
+    """Usage of list comprehansion to improve readability and ease of code
 
+    Args:
+        x (int): x coordinate of a cuboid
+        y (int): y coordinate of a cuboid
+        z (int): z coordinate of a cuboid
+        n (int): n is the number which acts as a filter, where sum of i+j+k
+            should not equal to n. where i, j, k are number range for x,y,z
+
+    Returns:
+        [list]: returns the list of numbers without the items in the filter.
+    """
     l_list = [
         [i, j, k]
         for i in range(x + 1)
