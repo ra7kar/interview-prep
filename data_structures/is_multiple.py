8e90b287d67fcb783242cf44a13e267011c5e776
ra7kar <ra7kar@gmail.com>
2021-12-16 20:11:27 -0600

    Return valuse corrected

--- data_structures/is_multiple.py
+++ data_structures/is_multiple.py
@@ -4,14 +4,17 @@
 
 
 def is_multiple(n, m):
-
+    ret_val = False
     for i in range(1, n + 1):
         if n / i == m:
-            ret_val = i
+            ret_val = True
 
-    return ret_val if ret_val else None
+    return ret_val
 
 
 if __name__ == "__main__":
 
-    print(is_multiple(100, 9))
+    n = 100
+    m = 5
+    print("The number is {}, which is divided by {}".format(n, m))
+    print("Multiple is {}".format(int(n / m) if is_multiple(n, m) else "None"))
--- data_structures/is_multiple_test.py
+++ data_structures/is_multiple_test.py
@@ -4,7 +4,9 @@ import pytest
 from is_multiple import is_multiple
 
 
-@pytest.mark.parametrize("n, m, result", [(100, 100, 1), (100, 50, 2), (50, 10, 5)])
+@pytest.mark.parametrize(
+    "n, m, result", [(100, 100, True), (100, 50, True), (50, 10, True), (100, 9, False)]
+)
 def test_is_multiple(n, m, result):
     ret_val = is_multiple(n, m)
 
