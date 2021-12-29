e2c8b7daa8b4149bcf68c4e61e01519b483f8871
ra7kar <ra7kar@gmail.com>
2021-12-28 22:22:06 -0600

    Bug fixed for file path

--- data_structures/dict_text_analysis_test.py
+++ data_structures/dict_text_analysis_test.py
@@ -8,8 +8,14 @@ from dict_text_analysis import text_analysis
 @pytest.mark.parametrize(
     "file_name, result",
     [
-        ("test1.txt", {"#": 1, "test": 2}),
-        ("test2.txt", {"#": 1, "test": 2, "check": 1, "this": 1, "out": 1}),
+        (
+            "/Users/rct/src/interview-prep/data_structures/test1.txt",
+            {"#": 1, "test": 2},
+        ),
+        (
+            "/Users/rct/src/interview-prep/data_structures/test2.txt",
+            {"#": 1, "test": 2, "check": 1, "this": 1, "out": 1},
+        ),
     ],
 )
 def test_dict_text_analysis(file_name, result):
