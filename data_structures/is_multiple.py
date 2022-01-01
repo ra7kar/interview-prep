4dc1c18fc029152dcbd41d3f1f705e4ddb77ba2c
ra7kar <ra7kar@gmail.com>
2022-01-01 16:31:50 -0600

    for loop removed and % operator used

--- data_structures/is_multiple.py
+++ data_structures/is_multiple.py
@@ -5,17 +5,15 @@
 
 def is_multiple(n, m):
     ret_val = False
-    for i in range(1, n + 1):
-        if n / i == m:
-            ret_val = True
-
+    if n % m == 0:
+        return True
     return ret_val
 
 
 if __name__ == "__main__":
 
-    n = 100
-    m = 5
+    n = 10
+    m = 10
 
     print("The number is {}, which is divided by {}".format(n, m))
     print("Multiple is {}".format(int(n / m) if is_multiple(n, m) else "None"))
