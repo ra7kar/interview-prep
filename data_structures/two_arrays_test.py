3f1981315962f001f53af4bb52bfd4fb1515b716
ra7kar <ra7kar@gmail.com>
2022-02-20 17:12:28 -0600

    Two arrays

--- data_structures/two_arrays_test.py
+++ data_structures/two_arrays_test.py
@@ -7,8 +7,8 @@ from two_arrays import two_arrays
 @pytest.mark.parametrize(
     "arr1, arr2, target, result",
     [
-        ([2, 5, 7, 8], [8, 5, 7, 1], 13, (2, 8)),
-        ([2, 5, 7, 8], [8, 5, 7, 1], 11, (5, 8)),
+        ([2, 5, 7, 8], [8, 5, 7, 1], 11, (2, 8)),
+        ([2, 5, 7, 8], [8, 5, 7, 1], 13, (5, 8)),
     ],
 )
 def test_two_arrays(arr1, arr2, target, result):
