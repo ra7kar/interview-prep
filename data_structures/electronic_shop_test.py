d14b2d9ae729cdb2a9f75de3dcd48361e9a18504
ra7kar <ra7kar@gmail.com>
2022-02-28 20:36:18 -0600

    Electronic shop, hacker rank problem

--- data_structures/electronic_shop_test.py
+++ data_structures/electronic_shop_test.py
@@ -7,7 +7,7 @@ from electronic_shop import get_money_spent
 
 @pytest.mark.parametrize(
     "keyboards, drives, b, result",
-    [([40, 50, 60], [5, 8, 12], 60, 58), ([3, 1], [5, 2, 8], 10, 9), ([4], [5], 9, -1)],
+    [([40, 50, 60], [5, 8, 12], 60, 58), ([3, 1], [5, 2, 8], 10, 9), ([4], [5], 5, -1)],
 )
 def test_get_money_spent(keyboards, drives, b, result):
     assert get_money_spent(keyboards, drives, b) == result
