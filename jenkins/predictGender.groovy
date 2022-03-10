90dbf10e98ff7c0a131740eef509a9bf61d003de
ra7kar <ra7kar@gmail.com>
2022-03-10 15:43:28 -0600

    Add Jenkinsfile for predicting gender

---
+++ jenkins/predictGender.groovy
@@ -0,0 +1,26 @@
+import groovy.json.JsonSlurper
+
+properties([
+    parameters([
+        string(name: 'name', defaultValue: 'Sam')
+    ])
+])
+
+def predictGender(String name) {
+    def get = new URL("https://api.genderize.io/?name=${name}").openConnection();
+    def getRC = get.getResponseCode();
+    if (getRC.equals(200)) {
+        def slurper = new groovy.json.JsonSlurper();
+        def result = slurper.parseText(get.getInputStream().getText());
+        return result.gender;
+    } else {
+        throw new Exception("HTTP Request to api.genderize.io failed");
+    }
+}
+
+node {
+    stage('Predict Gender') {
+        echo "Predicting Gender for: ${params.name}"
+        echo "Probability of prediction: ${predictGender(params.name)}"
+    }
+}
