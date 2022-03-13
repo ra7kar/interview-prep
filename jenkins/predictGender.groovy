f4b2e2bd762418d7f5678a1ec368908e684d213d
ra7kar <ra7kar@gmail.com>
2022-03-12 21:31:04 -0600

    predictGender function returns a tuple

--- jenkins/predictGender.groovy
+++ jenkins/predictGender.groovy
@@ -12,17 +12,17 @@ def predictGender(String name) {
     if (getRC.equals(200)) {
         def slurper = new groovy.json.JsonSlurper();
         def result = slurper.parseText(get.getInputStream().getText());
-        return [result.gender, result.count];
+        return ([result.gender, result.count]);
     } else {
         throw new Exception("HTTP Request to api.genderize.io failed");
     }
 }
 
 node {
-    [gender, count] = predictGender(params.name)
+    obj = predictGender(params.name)
     stage('Predict Gender') { 
         echo "Predicting Gender for: ${params.name}"
-        echo "Probability of prediction: ${gender}"
-        echo "Count: ${count}"
+        echo "Probability of prediction: ${obj[0]}"
+        echo "Count: ${obj[1]}"
     }
 }
