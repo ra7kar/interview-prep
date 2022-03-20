3e805ddba44d6d6fe34d2781c0c325403ac3c654
ra7kar <ra7kar@gmail.com>
2022-03-19 21:19:19 -0500

    Jenkins file updated

--- flask/app/app.py
+++ flask/app/app.py
@@ -4,9 +4,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/hello")
+@app.route("/")
 def hello():
     return "Hello, World!"
+
+
+@app.route("/bye")
+def bye():
+    return "Bye!"
 
 
 if __name__ == "__main__":
