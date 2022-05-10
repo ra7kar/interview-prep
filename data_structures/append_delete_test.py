6b8dbe8be7387f226f081187665edc9e42cfb131
ra7kar <ra7kar@gmail.com>
2022-05-10 15:44:45 -0500

    Append Delete, hacker rank problem.

--- data_structures/append_delete_test.py
+++ data_structures/append_delete_test.py
@@ -7,9 +7,9 @@ from append_delete import append_delete
 @pytest.mark.parametrize(
     "s, f, k, result",
     [
-        ("ashley, ash, 2", "No"),
-        ("hackerhappy, hackerrank, 9", "Yes"),
-        ("aba, aba, 7", "Yes"),
+        ("ashley", "ash", 2, "No"),
+        ("hackerhappy", "hackerrank", 9, "Yes"),
+        ("aba", "aba", 7, "Yes"),
     ],
 )
 def test_append_delete(s, f, k, result):
