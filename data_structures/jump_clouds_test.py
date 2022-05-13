be6b32bdde127a01db31a34c0e32b05b195b0ed1
ra7kar <ra7kar@gmail.com>
2022-05-13 10:25:58 -0500

    Jumping the clouds, hacker rank problem.

---
+++ data_structures/jump_clouds_test.py
@@ -0,0 +1,26 @@
+# pytest script for jumping the clouds, hacker rank problem.
+
+import pytest
+from jump_clouds import jump_clouds
+
+
+@pytest.mark.parametrize(
+    "c, result",
+    [
+        (
+            [
+                0,
+                0,
+                1,
+                0,
+                0,
+                1,
+                0,
+            ],
+            4,
+        ),
+        ([0, 0, 0, 1, 0, 0], 3),
+    ],
+)
+def test_jump_clouds(c, result):
+    assert jump_clouds(c) == result
