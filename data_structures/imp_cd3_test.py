cdb93fb6280b7ab136572b456e43e153fcc9d52b
ra7kar <ra7kar@gmail.com>
2022-05-20 18:51:56 -0500

    Simulate cd command, new version

---
+++ data_structures/imp_cd3_test.py
@@ -0,0 +1,23 @@
+# pytest script to test simulate cd command.
+
+import pytest
+from imp_cd3 import cd
+
+
+@pytest.mark.parametrize(
+    "cur_path, commands, result",
+    [
+        (
+            "/Users/rct/src/interview-prep/data_structures",
+            ["..", "../..", "test", "../", "test2"],
+            "Users/rct/test2",
+        ),
+        (
+            "/Users/rct/src/interview-prep/data_structures",
+            ["..", "../..", "test", "../"],
+            "Users/rct/test2",
+        ),
+    ],
+)
+def test_cd(cur_path, commands, result):
+    assert cd(cur_path, commands) == result
