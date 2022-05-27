cbf23b25718754dfdc38e1e9f0b531c768a5d91a
ra7kar <ra7kar@gmail.com>
2022-05-27 14:57:51 -0500

    Kaprekar numbers, hacker rank problem.

---
+++ data_structures/kaprekar_numbers.py
@@ -0,0 +1,42 @@
+# Kaprekar numbers, Hacker rank problem.
+# https://www.hackerrank.com/challenges/kaprekar-numbers/problem
+
+
+def kaprekar_numbers(p, q):
+
+    ret_val = []
+
+    for i in range(p, q + 1):
+
+        d = len(str(i))
+
+        sqr = str(i * i)
+
+        if len(sqr) < 2:
+            right = int(sqr)
+            left = 0
+        else:
+            r_len = d
+            right = int(sqr[-r_len:])
+            l_len = len(sqr) - d
+            left = int(sqr[:l_len])
+
+        if left + right == i:
+            ret_val.append(str(i))
+
+    if ret_val:
+        print(*ret_val)
+    else:
+        print("INVALID RANGE")
+
+
+def main():
+
+    p = 1
+    q = 700
+
+    kaprekar_numbers(p, q)
+
+
+if __name__ == "__main__":
+    main()
